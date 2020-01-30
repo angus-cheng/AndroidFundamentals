7fd7b7d9b0f5e8c1cf59ffb5448cf310524a2fb5
angus-cheng <anguscheng389@gmail.com>
2020-01-30 22:44:45 +1000

    Completion of 02.2 Activity lifecycle and state

--- TwoActivities/app/src/main/java/com/example/twoactivities/activity_second.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/activity_second.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 public class activity_second extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+    private static final String LOG_TAG = activity_second.class.getSimpleName();
 
     private EditText mReply;
 
@@ -23,6 +25,44 @@ public class activity_second extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     public void returnReply(View view) {
@@ -30,6 +70,7 @@ public class activity_second extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End activity_second");
         finish();
     }
 }
